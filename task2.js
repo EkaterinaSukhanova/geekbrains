62a948df80a7cb20d449fbdfd367dcb40a738ade
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-06-13 00:09:17 +0300

    work in two direction

--- task2.js
+++ task2.js
@@ -5,21 +5,30 @@ function min_way(str_1, str_2){
     if (str_1.length != str_2.length)
         return -1;
 
-    outer: for (var k = 0; k < str_1.length; ++k) {
-        var j = 0;
-        for (var i = k; i < str_1.length + k, j < str_2.length; ++i, ++j) {
+    // let b = str_1.length + 1;
+
+    outer: for (let k = 0; k < str_1.length; ++k) {
+        // b -= 1;
+        let j = 0;
+        for (let i = k; i < str_1.length + k, j < str_2.length; ++i, ++j) {
             if (str_1[i % str_1.length] != str_2[j]) {
                 continue outer;
             }
         }
-        return k;
+
+        let b = str_1.length - k;
+        if (k < b) {
+            return k;
+        }
+        else {
+            return b;
+        }
     }
+
     return -1;
 }
 
-
-var str_1 = process.argv[2];
-var str_2 = process.argv[3];
+let str_1 = process.argv[2];
+let str_2 = process.argv[3];
 
 console.log(min_way(str_1, str_2));
-

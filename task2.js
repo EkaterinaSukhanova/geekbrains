f68c895f7709362f4930f1c24e146ded031e6f78
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-06-13 12:54:57 +0300

    v1. with inner cycle

--- task2.js
+++ task2.js
@@ -1,17 +1,15 @@
 "use strict";
 
-function min_way(str_1, str_2){
+function min_way(str_1, str_2) {
 
-    if (str_1.length != str_2.length)
+    if (str_1.length !== str_2.length) {
         return -1;
-
-    // let b = str_1.length + 1;
+    }
 
     outer: for (let k = 0; k < str_1.length; ++k) {
-        // b -= 1;
-        let j = 0;
-        for (let i = k; i < str_1.length + k, j < str_2.length; ++i, ++j) {
-            if (str_1[i % str_1.length] != str_2[j]) {
+
+        for (let i = k, j = 0; i < str_1.length + k, j < str_2.length; ++i, ++j) {
+            if (str_1[i % str_1.length] !== str_2[j]) {
                 continue outer;
             }
         }
@@ -19,8 +17,7 @@ function min_way(str_1, str_2){
         let b = str_1.length - k;
         if (k < b) {
             return k;
-        }
-        else {
+        } else {
             return b;
         }
     }
